61767af8a53838d53b59dd8b1a7dcfbde3f76255
sawadogoclaver <sawadogoclaver@gmail.com>
2024-07-27 00:01:44 +0000

    Update results.js

--- node/results.js
+++ node/results.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
